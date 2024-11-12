8baee7566f43fbc2a8a830a93e83a8f290cf25ad
hasan-tavakoli <hassan.tavakoli1990@gmail.com>
2024-11-12 07:19:35 +0330

    readme.md

--- dags/bestseller_db_setup.py
+++ dags/bestseller_db_setup.py
@@ -1,30 +1,29 @@
 from cosmos import DbtDag, ProjectConfig, ProfileConfig, ExecutionConfig
 from cosmos.profiles import SnowflakeUserPasswordProfileMapping
 from airflow.datasets import Dataset
-import os
 from datetime import datetime
 
+def create_dbt_dag():
+    profile_config = ProfileConfig(
+        profile_name="bestseller",
+        target_name="dev",
+        profile_mapping=SnowflakeUserPasswordProfileMapping(
+            conn_id="dbt_to_snowflake",
+            profile_args={"database": "BESTSELLER", "schema": "DEV"},
+        ),
+    )
 
-profile_config = ProfileConfig(
-    profile_name="bestseller",
-    target_name="dev",
-    profile_mapping=SnowflakeUserPasswordProfileMapping(
-        conn_id="dbt_to_snowflake",
-        profile_args={"database": "BESTSELLER", "schema": "DEV"},
-    ),
-)
+    return DbtDag(
+        project_config=ProjectConfig("/usr/local/airflow/dags/dbt/bestseller"),
+        profile_config=profile_config,
+        execution_config=ExecutionConfig(
+            dbt_executable_path="/usr/local/airflow/dbt_venv/bin/dbt",
+        ),
+        schedule=[Dataset("SEED://publish_output_dataset")],
+        start_date=datetime(2023, 1, 1),
+        catchup=False,
+        dag_id="bestseller_db_setup",
+        default_args={"retries": 2},
+    )
 
-my_cosmos_dag = DbtDag(
-    project_config=ProjectConfig(
-        f"/usr/local/airflow/dags/dbt/bestseller",
-    ),
-    profile_config=profile_config,
-    execution_config=ExecutionConfig(
-        dbt_executable_path=f"/usr/local/airflow/dbt_venv/bin/dbt",
-    ),
-    schedule=[Dataset(f"SEED://publish_output_dataset")],
-    start_date=datetime(2023, 1, 1),
-    catchup=False,
-    dag_id="bestseller_db_setup",
-    default_args={"retries": 2},
-)
+dag = create_dbt_dag()
